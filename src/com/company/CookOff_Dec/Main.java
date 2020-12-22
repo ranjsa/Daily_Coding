235e0fec6577107504ea3a69d51bbacef9bf7c9a
Tushar <gauravtushar7@gmail.com>
2020-12-22 19:57:07 +0530

    Sort 0, 1, 2 array using dutch national flag algorithm

---
+++ src/com/company/CookOff_Dec/Main.java
@@ -0,0 +1,69 @@
+package com.company.CookOff_Dec;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public void solve() throws IOException {
+        int t = nextInt();
+        while(t --> 0) {
+
+        }
+    }
+
+    public void run() {
+        try {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(System.out);
+
+            solve();
+
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    BufferedReader br;
+    StringTokenizer in;
+    PrintWriter out;
+
+    public String nextToken() throws IOException {
+        while (in == null || !in.hasMoreTokens()) {
+            in = new StringTokenizer(br.readLine());
+        }
+        return in.nextToken();
+    }
+
+    public int nextInt() throws IOException {
+        return Integer.parseInt(nextToken());
+    }
+
+    public double nextDouble() throws IOException {
+        return Double.parseDouble(nextToken());
+    }
+
+    public long nextLong() throws IOException {
+        return Long.parseLong(nextToken());
+    }
+
+    public int[] nextArr(int n) throws IOException {
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = nextInt();
+        }
+        return res;
+    }
+
+    public static void main(String[] args) throws IOException {
+         new Main().run();
+    }
+}
--- src/com/company/DSA/Array/SortAnArrayWithThreeElement.java
+++ src/com/company/DSA/Array/SortAnArrayWithThreeElement.java
@@ -6,12 +6,56 @@ public class SortAnArrayWithThreeElement {
 
     public static void main(String[] args) {
         int[] array = {0, 2, 1, 2, 0};
-        sortArray(array);
+        //sortArray(array);
+          sort012(array);
+        //print the array
         for (int a : array) {
             System.out.print(a + " ");
         }
     }
-    // App
+    public static void swap(int i, int j, int[] array){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    //Dutch National Flag Algorithm OR 3-way Partitioning:
+    /*
+    * Keep three indices low = 1, mid = 1 and high = N and there are four ranges, 1 to low (the range containing 0), low to mid (the range containing 1), mid to high (the range containing unknown elements) and high to N (the range containing 2).
+      Traverse the array from start to end and mid is less than high. (Loop counter is i)
+      If the element is 0 then swap the element with the element at index low and update low = low + 1 and mid = mid + 1
+      If the element is 1 then update mid = mid + 1
+      If the element is 2 then swap the element with the element at index high and update high = high – 1 and update i = i – 1. As the swapped element is not processed
+      Print the output array.*/
+    public static void sort012(int[] a) {
+        int n = a.length;
+
+        int lo = 0;
+        int hi = n - 1;
+        int mid = 0;
+
+        while (mid <= hi) {
+            switch (a[mid]) {
+                case 0: {
+                   swap(lo, mid, a);
+                   lo++;
+                   mid++;
+                   break;
+                }
+                case 1: {
+                    mid++;
+                    break;
+                }
+                case 2: {
+                    swap(mid, hi, a);
+                    hi--;
+                    break;
+                }
+            }
+        }
+    }
+
+    // Approach-1:
     public static void sortArray(int[] array) {
            Arrays.sort(array);
     }
