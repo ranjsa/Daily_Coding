564f9d2a9605e33c786e750b30831b49f2023886
Tushar <gauravtushar7@gmail.com>
2020-12-24 23:59:02 +0530

    max Sum SubArray

---
+++ src/com/company/DSA/Array/LargestSumContigiousSubarray.java
@@ -0,0 +1,24 @@
+package com.company.DSA.Array;
+
+public class LargestSumContigiousSubarray {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,-2,5};
+
+        int maxSum = maxSumSubArray(array);
+        System.out.println(maxSum);
+    }
+
+    public static int maxSumSubArray(int[] array) {
+        int maxEndingHere = array[0];
+        int maxSoFar = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            int num = array[i];
+
+            maxEndingHere = Math.max(maxEndingHere + num, num);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+
+        return maxSoFar;
+    }
+}
