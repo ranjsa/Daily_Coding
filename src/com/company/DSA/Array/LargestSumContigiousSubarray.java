a70bc89d158cdb6be5136cb4eb8542a80ca41090
Tushar <gauravtushar7@gmail.com>
2020-12-25 19:01:04 +0530

    Minimize the height

--- src/com/company/DSA/Array/LargestSumContigiousSubarray.java
+++ src/com/company/DSA/Array/LargestSumContigiousSubarray.java
@@ -3,7 +3,6 @@ package com.company.DSA.Array;
 public class LargestSumContigiousSubarray {
     public static void main(String[] args) {
         int[] array = {1,2,3,-2,5};
-
         int maxSum = maxSumSubArray(array);
         System.out.println(maxSum);
     }
@@ -11,12 +10,13 @@ public class LargestSumContigiousSubarray {
     public static int maxSumSubArray(int[] array) {
         int maxEndingHere = array[0];
         int maxSoFar = array[0];
+        if (array.length > 1) {
+            for (int i = 1; i < array.length; i++) {
+                int num = array[i];
 
-        for (int i = 1; i < array.length; i++) {
-            int num = array[i];
-
-            maxEndingHere = Math.max(maxEndingHere + num, num);
-            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+                maxEndingHere = Math.max(maxEndingHere + num, num);
+                maxSoFar = Math.max(maxSoFar, maxEndingHere);
+            }
         }
 
         return maxSoFar;
---
+++ src/com/company/DSA/Array/MinimizeTheMaxDiffBwHeights.java
@@ -0,0 +1,63 @@
+package com.company.DSA.Array;
+
+import java.util.Arrays;
+
+public class MinimizeTheMaxDiffBwHeights {
+    public static void main(String[] args) {
+        int arr[] = {1, 5, 8, 10};
+        int n = arr.length;
+        int k = 2;
+        int maxDiff = getMaxDiff(arr, k, n);
+        System.out.println(maxDiff);
+    }
+
+    public static int getMaxDiff(int[] arr, int k, int n) {
+        //if there is only one element in the array
+        if (n == 1) {
+            return 0;
+        }
+
+        // Step: 1 Sort the array
+        Arrays.sort(arr);
+
+        //Initialize the Result
+        int ans = arr[n-1] - arr[0];
+
+        int sml = arr[0] + k;
+        int big = arr[n-1] - k;
+        int temp = 0;
+
+        if (sml > big) {
+            temp = sml;
+            sml = big;
+            big = temp;
+        }
+
+        //Traverse the middle elements
+        for (int i = 1; i < n-1; i++) {
+            int subtract = arr[i] - k;
+            int add = arr[i] + k;
+
+            //if both subraction and addition
+            //do not change diff
+            if (subtract >= sml || add <= big) {
+                continue;
+            }
+
+            //Either substraction causes a amaller number
+            // or addition causes a graeter number.
+            if (big - subtract <= add - sml) {
+                sml = subtract;
+            } else {
+                big = add;
+            }
+        }
+        return Math.min(ans, big - sml);
+    }
+
+    public static void swap(int i, int j, int[] array) {
+        int temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+    }
+}
