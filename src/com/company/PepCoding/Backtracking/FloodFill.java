95bb2413d925e131e86940aa2bd9aa684d7a253b
Tushar <gauravtushar7@gmail.com>
2021-03-10 09:35:56 +0530

    Backtracking > Flood Fill Self Written

--- src/com/company/PepCoding/Backtracking/FloodFill.java
+++ src/com/company/PepCoding/Backtracking/FloodFill.java
@@ -4,6 +4,47 @@ import java.util.Scanner;
 
 public class FloodFill {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        floodFill(arr);
+    }
+
+    public static void floodFill (int[][] arr) {
+        String psf = "";
+        floodFillHelper(arr, 0, 0, new boolean[arr.length][arr[0].length], psf);
+    }
+    public static void floodFillHelper (int[][] arr, int currR, int currC, boolean[][] visited, String psf) {
+        if (currR < 0 || currC < 0 || currR == arr.length || currC == arr[0].length || arr[currR][currC] == 1 || visited[currR][currC] == true) {
+            return;
+        }
+        if (currR == arr.length - 1 && currC == arr[0].length - 1) {
+            System.out.println(psf);
+        }
+        // mark current cell visited
+        visited[currR][currC] = true;
+        //send to top
+        floodFillHelper(arr, currR-1, currC, visited, psf + "t");
+        //send to left
+        floodFillHelper(arr, currR, currC-1, visited, psf + "l");
+        //send to bottom
+        floodFillHelper(arr, currR+1, currC, visited, psf + "d");
+        //send to right
+        floodFillHelper(arr, currR, currC+1, visited, psf + "r");
+
+        visited[currR][currC] = false;
+    }
+
+}
+
+/*
+ public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
@@ -42,5 +83,4 @@ public class FloodFill {
         // unmark
         visited[r][c] = false;
     }
-
-}
+ */
